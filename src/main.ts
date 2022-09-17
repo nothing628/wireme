64aa7c5e15350e164b01da2ec1d82f73d9721e68
nothing628 <yogyphang@gmail.com>
2022-09-17 17:12:36 +0700

    add pinia

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,9 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+import piniaPersist from "pinia-plugin-persist";
+import "./style.css";
+import App from "./App.vue";
 
-createApp(App).mount('#app')
+const pinia = createPinia();
+pinia.use(piniaPersist);
+createApp(App).use(pinia).mount("#app");
