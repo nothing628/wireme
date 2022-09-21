e0539d24ef06cfb76ab74ad4ca9d3ef504f95dd2
nothing628 <yogyphang@gmail.com>
2022-09-21 07:19:07 +0700

    add pwa support

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,36 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
-})
+  plugins: [
+    vue(),
+    VitePWA({
+      registerType: "autoUpdate",
+      injectRegister: "auto",
+      workbox: {
+        clientsClaim: true,
+        skipWaiting: true,
+      },
+      devOptions: {
+        enabled: true,
+      },
+      manifest: {
+        id: "/",
+        name: "Wireme",
+        short_name: "Wireme",
+        description: "Chat App",
+        theme_color: "#246BFD",
+        orientation: "portrait",
+        icons: [
+          {
+            src: "192.png",
+            sizes: '192x192',
+            type: 'image/png'
+          }
+        ]
+      }
+    }),
+  ],
+});
