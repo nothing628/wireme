b2f2b4801ff8b00d9db7ec24203bf95658998986
nothing628 <yogyphang@gmail.com>
2022-09-30 17:44:45 +0700

    add status page

---
+++ src/pages/status/StatusListPage.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        status list page
+    </div>
+</template>
--- src/router.ts
+++ src/router.ts
@@ -8,6 +8,18 @@ const routes: RouteRecordRaw[] = [
   {
     path: "/chat/:chat_id",
     component: () => import('./pages/MessagePage.vue'),
+  },
+  {
+    path: "/status",
+    component: () => import('./pages/status/StatusListPage.vue'),
+  },
+  {
+    path: "/status/new",
+    component: () => import('./pages/status/StatusCreatePage.vue'),
+  },
+  {
+    path: "/status/view/:status_id",
+    component: () => import('./pages/status/StatusViewPage.vue'),
   }
 ];
 
