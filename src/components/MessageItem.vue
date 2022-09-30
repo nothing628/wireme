82a9841eeaf1bab1873b78989060c1fbe286c02c
nothing628 <yogyphang@gmail.com>
2022-09-30 12:57:05 +0700

    add chat item

---
+++ src/components/MessageItem.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+defineProps<{
+    asSender?: boolean
+    isReaded?: boolean
+    message: string
+}>()
+</script>
+
+<template>
+    <div class="mb-5">
+        <div class="py-4 px-6 rounded-[20px] relative" :class="asSender ? 'bg-[#246BFD] text-white ml-10 rounded-br-md' : 'bg-[#E5E5E5] text-[#212121] mr-10 rounded-bl-md'">
+            <p>{{message}}</p>
+            <div class="inline absolute bottom-4 right-6">
+                <span class="font-medium text-xs">09:41</span>
+                <span class="inline ml-1" v-if="isReaded && asSender">
+                    <img class="inline-block w-4 h-4" src="../assets/IconCheck.png" />
+                </span>
+            </div>
+        </div>
+    </div>
+</template>
--- src/pages/MessagePage.vue
+++ src/pages/MessagePage.vue
@@ -2,6 +2,7 @@
 import GradientBackground from '../components/GradientBackground.vue';
 import MessageToolbar from '../components/MessageToolbar.vue';
 import MessageTypeInput from '../components/MessageTypeInput.vue';
+import MessageItem from '../components/MessageItem.vue';
 </script>
 
 <template>
@@ -9,6 +10,12 @@ import MessageTypeInput from '../components/MessageTypeInput.vue';
         <MessageToolbar />
     </GradientBackground>
     <div class="relative min-h-[calc(100vh-115px)]">
+        <div class="p-6">
+            <MessageItem message="Commodo commodo eiusmod quis anim." />
+            <MessageItem message="Consequat ullamco pariatur occaecat eu laborum." as-sender />
+            <MessageItem message="In quis dolore culpa esse dolore officia eiusmod fugiat id ad commodo quis voluptate sunt." is-readed/>
+            <MessageItem message="Ipsum labore qui aliquip aute dolor ad occaecat nisi mollit veniam deserunt velit esse." is-readed as-sender />
+        </div>
         <MessageTypeInput />
     </div>
 </template>
