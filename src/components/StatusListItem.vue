7ed36e7f3ceb7156e7cd39f244c92203ed17f91b
nothing628 <yogyphang@gmail.com>
2022-09-30 19:42:36 +0700

    add status list item component

---
+++ src/components/StatusListItem.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="flex items-center mb-6">
+        <div class="relative">
+            <img class="h-[60px] w-[60px] rounded-full border-[2.5px] border-[#246BFD]" src="../assets/GradientAvatar1.png"/>
+            <img class="w-[15px] h-[15px] absolute bottom-0 right-0" src="../assets/IconBluePlus.png" />
+        </div>
+        <div class="flex flex-col pl-5">
+            <p class="text-[#212121] text-base font-bold mb-1">Marielle Wigington</p>
+            <span class="text-[#616161] text-sm">10 minutes ago</span>
+        </div>
+    </div>
+</template>
--- src/pages/status/StatusListPage.vue
+++ src/pages/status/StatusListPage.vue
@@ -2,6 +2,9 @@
 import GradientBackground from '../../components/GradientBackground.vue';
 import MainToolbar from '../../components/MainToolbar.vue';
 import MainTabs from '../../components/MainTabs.vue';
+import StatusListContainer from '../../components/StatusListContainer.vue';
+import StatusListItem from '../../components/StatusListItem.vue';
+import StatusListDivider from '../../components/StatusListDivider.vue';
 </script>
 
 <template>
@@ -9,4 +12,16 @@ import MainTabs from '../../components/MainTabs.vue';
         <MainToolbar />
         <MainTabs :active-index="1"/>
     </GradientBackground>
+    <StatusListContainer>
+        <StatusListItem></StatusListItem>
+        <StatusListDivider>Recent updates</StatusListDivider>
+        <StatusListItem></StatusListItem>
+        <StatusListItem></StatusListItem>
+        <StatusListDivider>Viewed updates</StatusListDivider>
+        <StatusListItem></StatusListItem>
+        <StatusListItem></StatusListItem>
+        <StatusListDivider>Muted updates</StatusListDivider>
+        <StatusListItem></StatusListItem>
+        <StatusListItem></StatusListItem>
+    </StatusListContainer>
 </template>
