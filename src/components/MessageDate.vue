07eabd2bb50cda27746a05138bb37a5a3494d693
nothing628 <yogyphang@gmail.com>
2022-09-30 13:03:24 +0700

    add message date item

---
+++ src/components/MessageDate.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="mb-5">
+        <div class="py-0 px-6 w-full text-center">
+            <span class="bg-[#757575]/20 py-1 px-3 text-[#757575] text-xs rounded-md">
+                <slot></slot>
+            </span>
+        </div>
+    </div>
+</template>
--- src/pages/MessagePage.vue
+++ src/pages/MessagePage.vue
@@ -3,6 +3,7 @@ import GradientBackground from '../components/GradientBackground.vue';
 import MessageToolbar from '../components/MessageToolbar.vue';
 import MessageTypeInput from '../components/MessageTypeInput.vue';
 import MessageItem from '../components/MessageItem.vue';
+import MessageDate from '../components/MessageDate.vue';
 </script>
 
 <template>
@@ -11,6 +12,7 @@ import MessageItem from '../components/MessageItem.vue';
     </GradientBackground>
     <div class="relative min-h-[calc(100vh-115px)]">
         <div class="p-6">
+            <MessageDate>Today</MessageDate>
             <MessageItem message="Commodo commodo eiusmod quis anim." />
             <MessageItem message="Consequat ullamco pariatur occaecat eu laborum." as-sender />
             <MessageItem message="In quis dolore culpa esse dolore officia eiusmod fugiat id ad commodo quis voluptate sunt." is-readed/>
