ab791f9cbd9760d51ef3914e634c104dd0eb6215
nothing628 <yogyphang@gmail.com>
2022-09-30 12:19:04 +0700

    add message type input

---
+++ src/components/MessageTypeInput.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="px-6 pb-9 fixed bottom-0">
+        <div class="flex items-center">
+            <div class="flex items-center grow">
+                <button class="p-[6px] w-8 h-8 shrink-0">
+                    <img class="w-5 h-5" src="../assets/IconGraySmile.png" />
+                </button>
+                <input class="block w-full py-3 px-3 outline-none" placeholder="Type a message ..." />
+            </div>
+            <div class="flex items-center w-[132px] shrink-0">
+                <div>
+                    <button class="p-[6px]">
+                        <img class="w-5 h-5" src="../assets/IconGrayPaperclip.png" />
+                    </button>
+                    <button class="p-[6px] mr-[12px]">
+                        <img class="w-5 h-5" src="../assets/IconGrayCamera.png" />
+                    </button>
+                </div>
+                <button class="w-14 h-14 p-4 rounded-full bg-gradient-to-r from-[#5089FF] to-[#246BFD]">
+                    <img class="w-6 h-6" src="../assets/IconMic.png"/>
+                </button>
+            </div>
+        </div>
+    </div>
+</template>
--- src/pages/MessagePage.vue
+++ src/pages/MessagePage.vue
@@ -1,10 +1,14 @@
 <script lang="ts" setup>
 import GradientBackground from '../components/GradientBackground.vue';
 import MessageToolbar from '../components/MessageToolbar.vue';
+import MessageTypeInput from '../components/MessageTypeInput.vue';
 </script>
 
 <template>
     <GradientBackground>
         <MessageToolbar />
     </GradientBackground>
+    <div class="relative min-h-[calc(100vh-115px)]">
+        <MessageTypeInput />
+    </div>
 </template>
