46d8e619c4af2d35f9b052fb1eaaaf7e98d724fc
nothing628 <yogyphang@gmail.com>
2022-09-30 11:31:07 +0700

    add message toolbar

---
+++ src/components/MessageToolbar.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="flex pt-14 pb-5 px-6">
+        <div class="flex items-center grow">
+            <button class="p-[5px]">
+                <img class="w-7 h-7" src="../assets/IconArrowLeft.png" />
+            </button>
+            <div class="text-white font-semibold text-base ml-[10px]">
+                Someone names
+            </div>
+        </div>
+        <div class="">
+            <button class="p-[5px] ml-[10px]">
+                <img class="w-7 h-7" src="../assets/IconCall.png" />
+            </button>
+            <button class="p-[5px] ml-[10px]">
+                <img class="w-7 h-7" src="../assets/IconVideo.png" />
+            </button>
+            <button class="p-[5px] ml-[10px]">
+                <img class="w-7 h-7" src="../assets/IconMoreCircle.png" />
+            </button>
+        </div>
+    </div>
+</template>
--- src/pages/MessagePage.vue
+++ src/pages/MessagePage.vue
@@ -1,1 +1,10 @@
-<template>Hi</template>
+<script lang="ts" setup>
+import GradientBackground from '../components/GradientBackground.vue';
+import MessageToolbar from '../components/MessageToolbar.vue';
+</script>
+
+<template>
+    <GradientBackground>
+        <MessageToolbar />
+    </GradientBackground>
+</template>
